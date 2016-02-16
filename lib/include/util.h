354ed2473322dff11c7e2ace46ee791928cb2095
Sam Shelton <sam@shelt.ca>
2016-02-16 12:23:42 -0800

    Implement type-byte conversions
    Signed-off-by: Sam Shelton <sam@shelt.ca>

---
+++ lib/include/util.h
@@ -0,0 +1,11 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+void ustob(uint16_t src, uint8_t *dst);
+void uitob(uint32_t src, uint8_t *dst);
+
+uint16_t btous(uint8_t *src);
+uint32_t btoui(uint8_t *src);
+
+#endif /* UTIL_H */
+
---
+++ lib/src/util.c
@@ -0,0 +1,28 @@
+#include "util.h"
+
+void ustob(uint16_t src, uint8_t *dst)
+{
+    dst[0] = src >> 8 & 0xff;
+    dst[1] = src      & 0xff;
+}
+
+void uitob(uint32_t src, uint8_t *dst)
+{
+    dst[0] = src >> 24 & 0xff;
+    dst[1] = src >> 16 & 0xff;
+    dst[2] = src >> 8  & 0xff;
+    dst[3] = src       & 0xff;
+}
+
+uint16_t btous(uint8_t *src)
+{
+    return ntohs((uint16_t)((src[0] << 8) | src[1]));
+}
+
+uint32_t btoui(uint8_t *src)
+{
+    return ntohl((uint32_t)((src[0] << 24 |
+                             src[1] << 16 |
+                             src[2] << 8  |
+                             src[3])));
+}
