bb37461dcabc623944de848f357347ebc0adf98a
Sam Shelton <sam@shelt.ca>
2016-04-04 01:13:26 -0500

    Add other skeletons for other binaries
    Signed-off-by: Sam Shelton <sam@shelt.ca>

---
+++ admin/src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <io.h>
+#include <transactions.h>
+#include <util.h>
+#include <shared.h>
+#include <blocks.h>
+#include <crypto.h>
+
+
+int main(void)
+{
+    
+    return 0;
+}
